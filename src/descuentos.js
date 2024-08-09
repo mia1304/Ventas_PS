fa03ecb7af0799342cf87a8b5ff9a88a14b5bcb9
mia1304 <114312191+mia1304@users.noreply.github.com>
2024-08-09 16:03:11 -0500

    Update descuentos.js

--- src/descuentos.js
+++ src/descuentos.js
@@ -1,15 +1,12 @@
-// src/descuentos.js
-
 function applyDiscount(product, discountCode) {
-    if (discountCode && discountCode.isValid()) {
-      product.price -= discountCode.value;
-    } else if (!discountCode) {
-      throw new Error('Discount code is required');
-    } else {
-      throw new Error('Invalid discount code');
-    }
-    return product;
+  if (discountCode && discountCode.isValid()) {
+    product.price -= discountCode.value;
+  } else if (!discountCode) {
+    throw new Error('Discount code is required');
+  } else {
+    throw new Error('Invalid discount code');
   }
-  
-  module.exports = { applyDiscount };
-  
+  return product;
+}
+
+module.exports = { applyDiscount };
